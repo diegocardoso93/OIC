4f820b9b7d32bf47a9b5f8a04f5e0d301a8a8203
Diego Cardoso <dieg0card0s0@hotmail.com>
2018-10-01 01:18:42 -0300

    Update dragonboard_dependencies.sh

--- scripts/dragonboard_dependencies.sh
+++ scripts/dragonboard_dependencies.sh
@@ -1,3 +1,14 @@
+# lib mraa
 sudo add-apt-repository ppa:mraa/mraa
 sudo apt-get update
 sudo apt-get install libmraa2 libmraa-dev libmraa-java python-mraa python3-mraa node-mraa mraa-tools
+
+# lib libsoc
+
+sudo apt-get install autoconf build-essential libtool-bin pkg-config python-dev python3-dev
+git clone https://github.com/jackmitch/libsoc.git libsoc.git
+cd libsoc.git
+autoreconf -i
+./configure
+make
+make install
